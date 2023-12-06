42bfc7a116a343c911fde6617e1b03435b4ad42c
Ela Strinic <estrin00@fesb.hr>
2023-12-06 14:26:39 +0100

    footer begin

--- semester-project/components/Footer.tsx
+++ semester-project/components/Footer.tsx
@@ -1,7 +1,20 @@
-export default function Navbar(){
+export default function Footer(){
     return (
         <div>
-            
+            <hr className="bg-brown1 h-0.5" />
+            <div>
+                <p>ELINA</p>
+                <p>copyright © 2023</p>
+                <p>“Your Ultimate Jewelry Destination”</p>
+            </div>
+            <div>
+                <h1>SITEMAP</h1>
+                <p>Shop</p>
+                <p>About Us</p>
+                <p>Other</p>
+                <p>Shopping cart</p>
+                <p>Register/Log in</p>
+            </div>
         </div>
     )
 }
