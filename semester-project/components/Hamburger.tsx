e88d18b10050f16483cbd992bb3fbacbb7cec4d4
DinaVrandecic <dvrand01@fesb.hr>
2024-01-05 12:28:11 +0100

    hamburger fixed

--- semester-project/components/Hamburger.tsx
+++ semester-project/components/Hamburger.tsx
@@ -14,7 +14,7 @@ interface HamburgerProps {
   pages: Record<string, `/${string}`>;
 }
 const baseClass =
-  "uppercase whitespace-nowrap font-roboto-condensed text-base px-5 py-3 rounded-sm text-peach1 bg-nf_blue hover:bg-dark_blue hover:text-peach1";
+"uppercase whitespace-nowrap font-serif text-base px-5 py-3 rounded-sm text-dark_blue  hover:bg-dark_blue hover:text-peach1";
 
 const Hamburger: FC<HamburgerProps> = ({ pages }) => {
   const pathName = usePathname();
@@ -59,42 +59,52 @@ const toggleMenu = () => {
 
 
 
-
-
-        <div className=" hover:cursor-pointer">
-      <div
-        onClick={toggleMenu}
-        className="m-5 md:hidden lg:hidden flex flex-col gap-1 justify-around"
-      >
-        <div className="transition-all origin-center w-6 h-1 bg-brown1"></div>
-        <div className="transition-all origin-center w-6 h-1 bg-brown1"></div>
-        <div className="transition-all origin-center w-6 h-1 bg-brown1"></div>
+    <div className=" hover:cursor-pointer">
+      <div className="flex bg-nf_blue">
+        <div
+          onClick={toggleMenu}
+          className="p-5 bg-nf_blue md:hidden lg:hidden flex flex-col gap-1 justify-around"
+        >
+          <div className="transition-all origin-center w-6 h-1 bg-brown1"></div>
+          <div className="transition-all origin-center w-6 h-1 bg-brown1"></div>
+          <div className="transition-all origin-center w-6 h-1 bg-brown1"></div>
+        </div>
+        <div className="mx-[10px]  mt-[20px]">
+        <Image
+          src={logoImage}
+          alt="Hero Image"
+          width={100} 
+          height={100} 
+        />
+        </div>
       </div>
+
+
       <div
         className={` ${
           isMenuOpen ? "absolute" : "hidden"
-        }  w-full top-10 left-0 bg-dark_blue text-peach1 h-30 z-10`}
+        }  w-full top-10 left-0 bg-nf_blue text-dark_blue h-30 z-10 mt-[20px]`}
       >
-        <div className="flex flex-col justify-center items-center my-[20px]">
-        <ul className="flex gap-2">
-        {Object.entries(pages).map(([name, path]) => (
-          <li key={name}>
-            <Link href={path}>
-              <span
-                className={cn(baseClass, {
-                  "bg-dark_blue text-peach1 pointer-events-none":
-                    path === pathName,
-                })}
-              >
-                {name}
-              </span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+
+        <ul className="flex  flex-col justify-center items-center my-[20px] gap-[30px]">
+          {Object.entries(pages).map(([name, path]) => (
+            <li key={name}>
+              <Link href={path}>
+                <span
+                  className={cn(baseClass, {
+                    "bg-dark_blue text-peach1 pointer-events-none":
+                      path === pathName,
+                  })}
+                >
+                  {name}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
         </div>
       </div>
-    </div>
+
   );
 };
 export default Hamburger;
